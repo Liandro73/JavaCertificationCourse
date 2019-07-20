dbb4265a562038ffffd1f4faa39f0cbd49c9c916
Dernival Liandro <39772165+Liandro73@users.noreply.github.com>
2019-07-20 08:24:43 -0300

    Add files via upload

---
+++ javaoperators/Aula30.java
@@ -0,0 +1,19 @@
+package br.com.liandro.javaoperators;
+
+public class Aula30 {
+	
+	public static void main(String[] args) {
+		double d = 1.99999999998;
+		int i = (int)d;
+		System.out.println(i);
+		
+		long l = 19999594949594l;
+		int i2 = (int) l;
+		System.out.println(i2);
+		
+		int i3 = 245;
+		byte b = (byte)i3;
+		System.out.println(b);
+	}
+
+}
---
+++ javaoperators/Quests24A30.java
@@ -0,0 +1,15 @@
+package br.com.liandro.javaoperators;
+
+public class Quests24A30 {
+	
+	public static void main(String[] args) {
+		int i1 = 3/2;
+		double i2 = 3/2;
+		double i3 = 3/2.0;
+		
+		long x = 0; double d = 0;
+		double zero = x + d;
+		System.out.println(i1 + i2 + i3 + x + d + zero);
+	}
+
+}
