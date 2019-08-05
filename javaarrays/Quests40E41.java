8d156c1dfe2388c07c8cdf27a5b2b84a3a531ac2
Dernival Liandro <liandro.sys@gmail.com>
2019-08-05 00:19:18 -0300

    Adição das atividades das aulas 40 e 41

---
+++ javaarrays/Quests40E41.java
@@ -0,0 +1,15 @@
+package br.com.liandro.javaarrays;
+
+import java.util.ArrayList;
+
+public class Quests40E41 {
+	
+	public static void main(String[] args) {
+		ArrayList<String> c = new ArrayList<String>();
+		c.add("a");
+		c.add("a");
+		System.out.println(c.remove("a"));
+		System.out.println(c.size());
+	}
+
+}
