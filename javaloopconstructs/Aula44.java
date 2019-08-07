9ac680443db9d473a6f46faa78ab3f8e3ee9878c
Dernival Liandro <liandro.sys@gmail.com>
2019-08-07 00:02:04 -0300

    Adição da aula 44 e atividades

---
+++ javaloopconstructs/Aula44.java
@@ -0,0 +1,14 @@
+package br.com.liandro.javaloopconstructs;
+
+public class Aula44 {
+	
+	public static void main(String[] args) {
+		int i = 1;
+		
+		do  {
+			System.out.println(i);
+			i++;
+		} while (i < 10);
+	}
+
+}
---
+++ javaloopconstructs/Quests44.java
@@ -0,0 +1,11 @@
+package br.com.liandro.javaloopconstructs;
+
+public class Quests44 {
+	
+	public static void main(String[] args) {
+		boolean i = false;
+		do {
+			System.out.println(i);
+		} while(i);
+	}
+}
